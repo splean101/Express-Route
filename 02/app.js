ec0f588d8f5cbf6f202381a1c792e8ddcc219307
splean101 <40311839+splean101@users.noreply.github.com>
2019-06-27 14:13:22 +0300

    Add files via upload

--- 02/app.js
+++ 02/app.js
@@ -1,20 +1,29 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
-let storage = [];
-app.get('/data', (req, res) => {
-    for (key of storage) {
-        res.write(key);
-        res.send('That`s all!')
-    };
+
+app.get('/', (req, res) => {
+    fs.createReadStream('./index.html', 'utf8').on('data', (chunk) => {
+        res.write(chunk);
+    }).on('end', () => res.end());
 });
+
+app.get('/data', (req, res) => {
+    fs.createReadStream('./storage.txt', 'utf8').on('data', (chunk) => {
+        res.write(chunk);
+    }).on('end', () => res.end('storage is  read'));
+});
+
 app.post('/data', (req, res) => {
     req.on('data', (chunk) => {
-        storage.push(chunk);
+        fs.appendFile('./storage.txt', chunk + '\n', 'utf8', (err) => {
+            if (err) throw err;
+        });
     });
     req.on('end', () => {
-        res.send('Done!')
+        res.redirect('/');
     });
-})
+});
 
 app.listen(3000);
